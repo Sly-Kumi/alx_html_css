e2f58ab406ff8aaac73f9714249c7f68dfea7d51
Sly <sylvesterb.kumi@gmail.com>
2025-12-07 18:30:17 +0000

    Add the js

---
+++ headphones/8-script.js
@@ -0,0 +1,7 @@
+// Hamburger Toggle
+const hamburger = document.getElementById("hamburger");
+const mobileMenu = document.getElementById("mobile-menu");
+
+hamburger.addEventListener("click", () => {
+    mobileMenu.classList.toggle("open");
+});
